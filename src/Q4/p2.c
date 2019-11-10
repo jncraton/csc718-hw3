680f817ff4b88538272289e029d08f9d1412fb54
Jon Craton <jncraton@gmail.com>
2019-11-09 21:34:19 -0500

    Fixed runtime schedule

--- src/Q4/p2.c
+++ src/Q4/p2.c
@@ -11,13 +11,18 @@ int main (int argc, char *argv[])
 
 	/* Some initializations */
 	n = 1000000;
+
 	for (i=0; i < n; i++)
 		a[i] = b[i] = i * 1.0;
 
 	sum = 0.0;
 
-  	for (i=0; i < n; i++)
-		sum = sum + (a[i] * b[i]*b[i]*b[i]);
+  omp_set_num_threads(4);
+
+  #pragma omp parallel
+  #pragma omp for schedule(static,n) reduction ( +: sum )
+	for (i=0; i < n; i++)
+		sum += (a[i] * b[i]*b[i]*b[i]);
 
 	printf("   Sum = %f\n",sum);
 }
