9280ebea3c0943754d1d674c9e26d07cbb32c20f
Jon Craton <jncraton@gmail.com>
2019-11-14 15:59:58 -0500

    P4 attempt

--- src/Q4/p4.c
+++ src/Q4/p4.c
@@ -21,34 +21,32 @@ chunk = 10;                    /* set loop iteration chunk size */
   
   omp_set_num_threads(4);
 
-  #pragma omp parallel for
-  for (i=0; i<NRA; i++)
+  //#pragma omp parallel for
+  for (i=0; i<NRA; i++) {
     for (j=0; j<NCA; j++)
       a[i][j]= i+j;
-
-  #pragma omp critical
-
-  #pragma omp parallel for
-  for (i=0; i<NCA; i++)
+  }
+  
+  //#pragma omp parallel for
+  for (i=0; i<NCA; i++) {
     for (j=0; j<NCB; j++)
       b[i][j]= i*j;
-
-  #pragma omp critical
+  }
 
   #pragma omp parallel for
-  for (i=0; i<NRA; i++)
+  for (i=0; i<NRA; i++) {
     for (j=0; j<NCB; j++)
       c[i][j]= 0;
-
-  #pragma omp critical
+  }
 
   for (i=0; i<NRA; i++)
     {
     for(j=0; j<NCB; j++)
-      for (k=0; k<NCA; k++)
+      #pragma omp parallel for
+      for (k=0; k<NCA; k++) {
         c[i][j] += a[i][k] * b[k][j];
+      }
     }
-
 
 /*** Print results ***/
 printf("******************************************************\n");
